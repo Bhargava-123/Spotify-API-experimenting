2cde3c2b3b6f46349ff187bc0f80b88f3796e2d3
Bhargava-123 <bhargavashankar2003@gmail.com>
2023-03-16 19:50:50 +0530

    user login and data retrieval done

--- spmoodfinder/spmoodfinder/views.py
+++ spmoodfinder/spmoodfinder/views.py
@@ -1,70 +1,64 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import CacheHandler, CacheFileHandler
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+import requests
+
 
 client_id = "6619410f74d84543b61f09853f7259cb"
 client_secret = "79a0c90081ac46029087c91521d7fa30"
 token_info = "token_info"
-
 def home(request):
     global sp_oauth
     sp_oauth = create_spotify_oauth(request)
-    auth_url = sp_oauth.get_authorize_url()
+    auth_url = sp_oauth.get_authorize_url() #AUTHORIZE/
     return redirect(auth_url)
-    
-    # return redirectView(auth_url) 
-    # return HttpResponse("Hello "+str(request.build_absolute_uri()))
-
 def getTracksView(request):
-
-    response_string = str()
+    result_string = ""
     token_info = get_token(request)
-    print("\n\nTHIS IS THE ACCESS TOKEN:\n#######################\n",token_info['access_token']+"\n\n")
     token = token_info['access_token']
+    #print("Access Token",token)
     sp = spotipy.Spotify(auth=token)
-    for sp_range in ['short_term', 'medium_term', 'long_term']:
-        print("range:", sp_range)
-        results = sp.current_user_top_artists(time_range=sp_range, limit=50)
-        for i, item in enumerate(results['items']):
-            response_string += item['name']+"<br>"
-            print(i,item['name'])
-    return HttpResponse("<h1>{}</h1>".format(response_string))
+    user_name = str(sp.current_user()['display_name'])
+    results = sp.current_user_playlists()
+    for i, item in enumerate(results['items']):
+        print(i, item['name'])
+        result_string += item['name']
+    context = {
+        'data': result_string
+    }
+    return render(requests,'index.html',context = context)
 
 def get_token(request):
     token_info = request.session.get('token_info')
     return token_info
 
 def redirectView(request):
-    request.session.flush()
+    #SECOND CALL TO GET ACCESS TOKEN
     code = request.GET.get('code')
-    token_info = sp_oauth.get_access_token(code)
-    request.session["token_info"] = token_info
-
+    print(code)
+    token_info = sp_oauth.get_access_token(code,check_cache=False)
+    request.session["token_info"] = token_info 
+    print(token_info)
     response = redirect("http://127.0.0.1:8000/getTracks")
     return response
     
-
+##create spotify OAuth
 def create_spotify_oauth(request):
     redirect_url = request.build_absolute_uri() + str("redirect")
     return SpotifyOAuth(
         client_id = client_id,
         client_secret = client_secret,
         redirect_uri = redirect_url,
-        scope = "user-library-read user-top-read"
+        scope = "user-top-read user-read-email user-read-private"
     )
 
 def logoutView(request):
     return HttpResponse("you have logged out")
-# def logout(request):
-#     request.session['token_info'] = "{ }"
-#     # token_info = get_token(request)
-#     # print("\n\nTHIS IS THE ACCESS TOKEN FROM LOGOUT:\n#######################\n",token_info+"\n\n")
-#     response = redirect("http://accounts.spotify.com/logout")
-#     #response1 = redirect("http://127.0.0.1:8000/")
-#     return response
 def logout(request):
     for key in list(request.session.keys()):
         request.session.pop(key)
     print(request.session)
-    return redirect('/logoutView')
+    request.session.flush()
+    return redirect('http://accounts.spotify.com/logout')
